d16ffd369a86d5e49493bc6427530d5440661db1
Harrison <given73@gmail.com>
2021-09-01 21:11:32 +0900

    Add files via upload

---
+++ BandpassFilterButterworthImplementation.cs
@@ -0,0 +1,34 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+//namespace ButterWorth_Filter
+//{
+//    class BandpassFilterButterworthImplementation
+//    {
+//    }
+//}
+using System;
+
+namespace DSP
+{
+    public class BandpassFilterButterworthImplementation
+    {
+        protected LowpassFilterButterworthImplementation lowpassFilter;
+        protected HighpassFilterButterworthImplementation highpassFilter;
+
+        public BandpassFilterButterworthImplementation(double bottomFrequencyHz, double topFrequencyHz, int numSections, double Fs)
+        {
+            this.lowpassFilter  = new LowpassFilterButterworthImplementation(topFrequencyHz, numSections, Fs);
+            this.highpassFilter = new HighpassFilterButterworthImplementation(bottomFrequencyHz, numSections, Fs);
+        }
+
+        public double compute(double input)
+        {
+            // compute the result as the cascade of the highpass and lowpass filters
+            return this.highpassFilter.compute(this.lowpassFilter.compute(input));
+        }
+    }
+}
